f0c4260435e5ecda8bf4d407db2e3349af194f6f
Yoshin666 <joshua0307@outlook.com>
2025-11-20 00:40:10 +0100

    Update App.jsx

--- Earth-DND/src/App.jsx
+++ Earth-DND/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";

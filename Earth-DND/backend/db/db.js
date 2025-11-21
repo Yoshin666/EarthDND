77ec49f416eb448b03c829ce35dadace9b0ab76e
Joshua Esteban <joshua0307@outlook.com>
2025-11-21 02:09:24 +0100

    environment update

--- Earth-DND/backend/db/db.js
+++ Earth-DND/backend/db/db.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2");
 const connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASS,
-  database: process.env.MYSQL_DB,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQL_ROOT_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQL_PORT || 3306,
 });
 

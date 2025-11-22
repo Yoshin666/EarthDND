19d508e4dc83b746591c5e5a39ac84bc70a789a8
Joshua Esteban <joshua0307@outlook.com>
2025-11-23 00:03:29 +0100

    a

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,8 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 export function Login({ onClose }) {
-  const API_URL = import.meta.env.VITE_API_URL;
-  console.log("API_URL en Login:", API_URL);
+  const API_URL = "https://earthdnd.onrender.com";
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
   const [error, setError] = useState("");
